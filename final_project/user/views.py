9e9364c3f383816351f44bd501216c049ea26ba4
Syong Kim <watdran@korea.com>
2022-09-05 23:33:24 +0900

    new file:   db.sqlite3 	new file:   final_project/__init__.py 	new file:   final_project/__pycache__/__init__.cpython-38.pyc 	new file:   final_project/__pycache__/settings.cpython-38.pyc 	new file:   final_project/__pycache__/urls.cpython-38.pyc 	new file:   final_project/__pycache__/wsgi.cpython-38.pyc 	new file:   final_project/asgi.py 	new file:   final_project/settings.py 	new file:   final_project/urls.py 	new file:   final_project/wsgi.py 	new file:   manage.py 	new file:   templates/base.html 	new file:   templates/index.html 	new file:   templates/login.html 	new file:   templates/register.html 	new file:   templates/users.html 	new file:   user/__init__.py 	new file:   user/__pycache__/__init__.cpython-38.pyc 	new file:   user/__pycache__/admin.cpython-38.pyc 	new file:   user/__pycache__/apps.cpython-38.pyc 	new file:   user/__pycache__/forms.cpython-38.pyc 	new file:   user/__pycache__/models.cpython-38.pyc 	new file:   user/__pycache__/views.cpython-38.pyc 	new file:   user/admin.py 	new file:   user/apps.py 	new file:   user/forms.py 	new file:   user/migrations/0001_initial.py 	new file:   user/migrations/__init__.py 	new file:   user/migrations/__pycache__/0001_initial.cpython-38.pyc 	new file:   user/migrations/__pycache__/__init__.cpython-38.pyc 	new file:   user/models.py 	new file:   user/tests.py 	new file:   user/views.py

---
+++ final_project/user/views.py
@@ -0,0 +1,69 @@
+from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import LoginForm, RegisterForm
+
+User = get_user_model()
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("login")
+    else:
+        logout(request)
+        form = RegisterForm()
+    return render(request, "register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            raw_password = form.cleaned_data.get("password")
+            remember_me = form.cleaned_data.get("remember_me")
+            msg = "올바른 유저ID와 패스워드를 입력하세요."
+            try:
+                user = Users.objects.get(email=email)
+            except Users.DoesNotExist:
+                pass
+            else:
+                if user.check_password(raw_password):
+                    msg = None
+                    login(request, user)
+                    is_ok = True
+                    request.session["remember_me"] = remember_me
+
+                    # if not remember_me:
+                    #     request.session.set_expiry(0)					
+    else:
+        msg = None
+        form = LoginForm()
+    print("REMEMBER_ME: ", request.session.get("remember_me"))
+    return render(request, "login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
+@login_required
+def user_list_view(request):
+    page = int(request.GET.get("p", 1))
+    users = Users.objects.all().order_by("-id")
+    paginator = Paginator(users, 10)
+    users = paginator.get_page(page)
+
+    return render(request, "users.html", {"users": users})
+
+
